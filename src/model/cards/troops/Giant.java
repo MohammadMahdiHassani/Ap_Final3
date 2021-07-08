5f3e6073804fa81737b6e9188fea08a9a0f5f129
Moxitron <70322560+moxitron@users.noreply.github.com>
2021-07-08 22:46:45 +0430

    writing Giant levels enum

--- src/model/cards/troops/Giant.java
+++ src/model/cards/troops/Giant.java
@@ -1,4 +1,31 @@
-package com.company.model.cards.troops;
+package model.cards.troops;
 
 public class Giant {
+
+
+    private enum Level {
+        LEVEL_1(126,2000) ,
+        LEVEL_2(138,2200) ,
+        LEVEL_3(152,2420) ,
+        LEVEL_4(167,2660) ,
+        LEVEL_5(183,2920) ;
+
+        private int damage ;
+        private int hitPoint ;
+
+        private Level(int damage , int hitPoint){
+            this.damage = damage ;
+            this.hitPoint = hitPoint;
+        }
+
+        public int getDamage() {
+            return damage;
+        }
+
+        public int getHitPoint() {
+            return hitPoint;
+        }
+    }
+
+
 }
