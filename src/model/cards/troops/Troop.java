0cbbcdc6e2c048c8d143bea00d640b175eeecf18
Moxitron <70322560+moxitron@users.noreply.github.com>
2021-07-09 00:11:15 +0430

    modifying Troop and Level - Level (0)

--- src/model/cards/troops/Troop.java
+++ src/model/cards/troops/Troop.java
@@ -1,17 +1,16 @@
-package com.company.model.cards.troops;
+package model.cards.troops;
 
-import com.company.model.cards.troops.enums.Level;
-import com.company.model.cards.troops.enums.Speed;
-import com.company.model.cards.troops.enums.Target;
+
+
 
 public abstract class Troop {
-    private Level level ;
+
 
     private int hitPoint ;
     private int damage ;
-    private Speed speed ;
+    private com.company.model.cards.troops.enums.Speed speed ;
     private int hitSpeed ;
-    private Target target ;
+    private com.company.model.cards.troops.enums.Target target ;
     private int cost ;
     private int count ;
 
