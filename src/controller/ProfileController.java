bdbb3699fc0d2e0f868efa775d00d7c226d9f566
Moxitron <70322560+moxitron@users.noreply.github.com>
2021-07-08 19:04:08 +0430

    writing actionHandler in ProfileController

--- src/controller/ProfileController.java
+++ src/controller/ProfileController.java
@@ -1,4 +1,57 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ProfileController {
+
+    @FXML
+    private ImageView gameHistory;
+
+    @FXML
+    private ImageView battleDeck;
+
+    @FXML
+    private ImageView mainPage;
+
+    @FXML
+    private ProgressBar XPprogressSlider;
+
+    @FXML
+    private Label TroopyCounter;
+
+
+    @FXML
+    void actionHandler(MouseEvent event) throws IOException {
+        String fxmlAddress = getFxml(event);
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlAddress));
+        Stage stage = (Stage) mainPage.getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+
+
+    }
+    private String getFxml(MouseEvent event){
+        if(event.getSource() == mainPage){
+            return "../view/Menu.fxml";
+        }
+        else if(event.getSource() == gameHistory){
+            return "../view/gameHistory.fxml";
+        }
+        else if(event.getSource() == battleDeck){
+            return "../view/Deck.fxml";
+        }
+        else
+            return "";
+    }
 }
