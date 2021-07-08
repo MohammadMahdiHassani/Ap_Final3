6079da153b4f42a8d26e8c247afb7ae1438569ea
Moxitron <70322560+moxitron@users.noreply.github.com>
2021-07-08 19:59:34 +0430

    writing ProfileController actionHandler

--- src/controller/ProfileController.java
+++ src/controller/ProfileController.java
@@ -32,6 +32,7 @@ public class ProfileController {
 
     @FXML
     void actionHandler(MouseEvent event) throws IOException {
+
         String fxmlAddress = getFxml(event);
         Parent root = FXMLLoader.load(getClass().getResource(fxmlAddress));
         Stage stage = (Stage) mainPage.getScene().getWindow();
