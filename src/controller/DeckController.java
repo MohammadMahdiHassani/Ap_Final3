63ebdd874872f5b3bd6c1b42fbb447c0b375eae6
mohammad mahdi hassani <mmh2116@chmail.ir>
2021-07-09 17:10:36 +0330

    deck is ok and pages connected to each other

--- src/controller/DeckController.java
+++ src/controller/DeckController.java
@@ -5,21 +5,28 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 import model.cards.Army;
+
+import java.io.IOException;
 
 
 public class DeckController {
 
+    @FXML
+    private ImageView profilePage;
+    @FXML
+    private ImageView doneIcon;
     @FXML
     private ImageView gameHistory;
 
@@ -46,6 +53,8 @@ public class DeckController {
 
     private ObservableList<Army> allArmies =
             FXCollections.observableArrayList();
+    private Army mainLastValue;
+    private Army allLastValue;
 
     public void initialize() {
         mainArmies.add(new Army("/view/photos/archers.png", "archer"));
@@ -64,31 +73,32 @@ public class DeckController {
 
         allArmy.setItems(allArmies);
         mainArmy.setItems(mainArmies);
-
+        mainArmy.edit(0);
         mainArmy.getSelectionModel().selectedItemProperty().addListener(
                 new ChangeListener<Army>() {
                     @Override
-                    public synchronized void changed(ObservableValue<? extends Army> observable, Army oldValue, Army newValue) {
+                    public void changed(ObservableValue<? extends Army> observable, Army oldValue, Army newValue) {
                         allArmy.getItems().add(newValue);
-                        mainArmy.getItems().remove(newValue);
+                        mainArmy.getItems().remove(oldValue);
+                        mainLastValue = newValue;
+                        //System.out.println(newValue.getName());
                         //System.out.println(mainArmies.size());
                     }
                 }
         );
 
+
         allArmy.getSelectionModel().selectedItemProperty()
                 .addListener(
                         new ChangeListener<Army>() {
                             @Override
-                            public synchronized void changed(ObservableValue<? extends Army> observable, Army oldValue, Army newValue) {
+                            public void changed(ObservableValue<? extends Army> observable, Army oldValue, Army newValue) {
                                 mainArmy.getItems().add(newValue);
-                                allArmy.getItems().remove(newValue);
-
+                                allArmy.getItems().remove(oldValue);
+                                allLastValue = newValue;
                             }
                         }
                 );
-
-
         mainArmy.setCellFactory(
                 new Callback<ListView<Army>, ListCell<Army>>() {
                     @Override
@@ -109,8 +119,59 @@ public class DeckController {
     }
 
     @FXML
-    void actionHandler(MouseEvent event) {
+    void endClick(MouseEvent event) {
+        int mainLastIndex = -1;
+        int allLastIndex = -1;
+        if (mainLastValue != null) {
+            for (int i = 0; i < mainArmy.getItems().size(); i++) {
+                if (mainArmy.getItems().get(i).getName().equals(mainLastValue.getName())) {
+                    mainLastIndex = i;
+                    break;
+                }
+            }
+        }
+        if (allLastValue != null) {
+            for (int i = 0; i < allArmy.getItems().size(); i++) {
+                if (allArmy.getItems().get(i).getName().equals(allLastValue.getName())) {
+                    allLastIndex = i;
+                    break;
+                }
+            }
+        }
+        if (allArmy.getItems().size() > allLastIndex && allLastIndex >= 0) {
+            allArmy.getItems().remove(allLastIndex);
+            mainArmy.getItems().remove(mainArmy.getItems().size() - 1);
+        }
+        if (mainArmy.getItems().size() > mainLastIndex && mainLastIndex >= 0) {
+            mainArmy.getItems().remove(mainLastIndex);
+            allArmy.getItems().remove(allArmy.getItems().size() - 1);
+        }
+    }
+
+    @FXML
+    void actionHandler(MouseEvent event) throws IOException {
+        String fxmlAddress = getFxml(event);
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlAddress));
+        Stage stage = (Stage) mainPage.getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        stage.show();
+
 
     }
+    private String getFxml(MouseEvent event){
+        if(event.getSource() == mainPage){
+            return "../view/Menu.fxml";
+        }
+        else if(event.getSource() == gameHistory){
+            return "../view/BattleHistory.fxml";
+        }
+        else if(event.getSource() == profilePage){
+            return "../view/Profile.fxml";
+        }
+        else
+            return "";
+    }
+
 
 }
--- src/model/cards/Army.java
+++ src/model/cards/Army.java
@@ -12,6 +12,14 @@ public class Army {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public Image getImage() {
         return image;
     }
