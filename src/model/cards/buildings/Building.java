f19bb40e7f44d3bcee65574c735f7ad8c66f6747
Moxitron <70322560+moxitron@users.noreply.github.com>
2021-07-10 14:18:00 +0430

    wrinting Buildings and adding Cannon

--- src/model/cards/buildings/Building.java
+++ src/model/cards/buildings/Building.java
@@ -1,7 +1,19 @@
 package model.cards.buildings;
 
+import javafx.scene.image.Image;
 import model.cards.Card;
+import model.cards.troops.enums.Target;
 
 public abstract class Building extends Card {
-    
+    private float hitSpeed ;
+    private Target target ;
+    private int rang ;
+    private int lifeTime ;
+    public Building(Image image, int cost, int rang , Target target , float hitSpeed , int lifeTime) {
+        super(image, cost);
+        this.hitSpeed = hitSpeed ;
+        this.lifeTime = lifeTime ;
+        this.target = target ;
+        this.rang = rang ;
+    }
 }
