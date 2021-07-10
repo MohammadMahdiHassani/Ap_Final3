1fc97c163af2208ab6ca117845b162c48e0ec775
Moxitron <70322560+moxitron@users.noreply.github.com>
2021-07-10 16:58:56 +0430

    finishing writing of Barbarian

--- src/model/cards/troops/Barbarian.java
+++ src/model/cards/troops/Barbarian.java
@@ -1,34 +1,18 @@
 package model.cards.troops;
 
 import javafx.scene.image.Image;
-import model.cards.troops.Archer;
+
+import model.cards.levelEnums.BarbarianLevel;
 import model.cards.troops.enums.Speed;
 import model.cards.troops.enums.Target;
 
 public class Barbarian extends Troop{
-    private Level level;
+    private BarbarianLevel level;
     private static final Image image = new Image("/view/photos/giant.png");
 
-    public Barbarian(Level level) {
-        super(image, level.hitPoint, level.damage, Speed.SLOW, 1.5f, Target.GROUND, 5, 1);
+    public Barbarian(BarbarianLevel level) {
+        super(image, level.getHitPoint(), level.getDamage(), Speed.MEDIUM, 1.5f, Target.GROUND, 5, 4);
         this.level = level ;
-    }
-
-    private enum Level {
-        LEVEL_1(126, 2000),
-        LEVEL_2(138, 2200),
-        LEVEL_3(152, 2420),
-        LEVEL_4(167, 2660),
-        LEVEL_5(183, 2920);
-
-        private int damage;
-        private int hitPoint;
-
-        private Level(int damage, int hitPoint) {
-            this.damage = damage;
-            this.hitPoint = hitPoint;
-        }
-
     }
 
 }
