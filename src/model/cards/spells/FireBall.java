295d3d55dd9658ed4d7294c4f69c2b2ced60004b
Moxitron <70322560+moxitron@users.noreply.github.com>
2021-07-10 14:17:24 +0430

    writing Spell and adding Arrows , FireBall , Rage spells

---
+++ src/model/cards/spells/FireBall.java
@@ -0,0 +1,9 @@
+package model.cards.spells;
+
+import javafx.scene.image.Image;
+
+public class FireBall extends Spell {
+    public FireBall(Image image, int cost, int radius) {
+        super(image, cost, radius);
+    }
+}
--- src/model/cards/spells/Spell.java
+++ src/model/cards/spells/Spell.java
@@ -1,6 +1,13 @@
 package model.cards.spells;
 
+
+import javafx.scene.image.Image;
 import model.cards.Card;
 
 public abstract class Spell extends Card {
+    private int radius ;
+    public Spell(Image image, int cost , int radius) {
+        super(image, cost);
+        this.radius = radius ;
+    }
 }
