0646cb52a8f2a35dc507e22d19462acbc204c575
mohammad mahdi hassani <mmh2116@chmail.ir>
2021-07-12 18:25:04 +0330

    the view of timer and next card and progress bar of elixir

--- src/view/ArenaView.java
+++ src/view/ArenaView.java
@@ -1,8 +1,12 @@
 package view;
 
 import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.effect.Shadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import model.GameElement;
 import model.cards.CellValue;
 import model.game.ArenaModel;
@@ -13,14 +17,17 @@ import static model.cards.CellValue.*;
 
 public class ArenaView extends Group {
     public final static double CELL_WIDTH = 26.0;
+    private Label timeLabel;
     private int rowCount;
     private int columnCount;
     private ImageView[][] cellView;
     private ImageView[][] componentView;
+
     public ArenaView() {
     }
 
     private void initializeGrid() {
+        timeLabel = new Label("3:45");
         cellView = new ImageView[rowCount][columnCount];
         componentView = new ImageView[rowCount][columnCount];
         for (int row = 0; row < rowCount; row++) {
@@ -41,7 +48,15 @@ public class ArenaView extends Group {
                 this.getChildren().add(imageView1);
             }
         }
+        timeLabel.setLayoutX((CELL_WIDTH * 17) + 6 );
+        timeLabel.setLayoutY(CELL_WIDTH * 0);
+        timeLabel.setTextFill(Color.ANTIQUEWHITE);
+        timeLabel.setFont(Font.font("adobe heiti std R",18));
+        this.getChildren().add(timeLabel);
+
+
     }
+
     public void setBackgroundCell(ArenaModel model) {
         CellValue[][] cellValues = null;
         cellValues = model.getBackGroundCellValues();
@@ -58,43 +73,38 @@ public class ArenaView extends Group {
                     cellView[i][j].setImage(STONE.getThumbnailImage());
                 } else if (cellValues[i][j] == CellValue.SHRUB) {
                     cellView[i][j].setImage(SHRUB.getThumbnailImage());
-                }
-                else if (cellValues[i][j] == TREE)
-                {
+                } else if (cellValues[i][j] == TREE) {
                     cellView[i][j].setImage(TREE.getThumbnailImage());
                 } else if (cellValues[i][j] == CellValue.FENCE) {
                     cellView[i][j].setImage(FENCE.getThumbnailImage());
                 } else if (cellValues[i][j] == CellValue.HOME) {
                     cellView[i][j].setImage(HOME.getThumbnailImage());
+                } else if (cellValues[i][j] == CellValue.TIME) {
+                    cellView[i][j].setImage(TIME.getThumbnailImage());
                 }
             }
         }
     }
+
     public void update(ArenaModel model) {
         GameElement[][] cellValues = null;
-            cellValues = model.getCellValues();
+        cellValues = model.getCellValues();
 
         for (int i = 0; i < rowCount; i++) {
             for (int j = 0; j < columnCount; j++) {
-                if(cellValues[i][j] == null){
+                if (cellValues[i][j] == null) {
                     componentView[i][j].setImage(null);
-                }
-                else if (cellValues[i][j].getValue() == CellValue.KINGTOWER) {
+                } else if (cellValues[i][j].getValue() == CellValue.KINGTOWER) {
                     componentView[i][j].setImage(KINGTOWER.getThumbnailImage());
-                }
-                else if (cellValues[i][j].getValue() == CellValue.ARCHERTOWER) {
+                } else if (cellValues[i][j].getValue() == CellValue.ARCHERTOWER) {
                     componentView[i][j].setImage(ARROWS.getThumbnailImage());
-                }
-                else if (cellValues[i][j].getValue() == GIANT) {
+                } else if (cellValues[i][j].getValue() == GIANT) {
                     componentView[i][j].setImage(GIANT.getThumbnailImage());
-                }
-                else if (cellValues[i][j].getValue() == ARCHER) {
+                } else if (cellValues[i][j].getValue() == ARCHER) {
                     componentView[i][j].setImage(ARCHER.getThumbnailImage());
-                }
-                else if (cellValues[i][j].getValue() == CANNON) {
+                } else if (cellValues[i][j].getValue() == CANNON) {
                     componentView[i][j].setImage(CANNON.getThumbnailImage());
-                }
-                else if (cellValues[i][j].getValue() == BARBERIAN) {
+                } else if (cellValues[i][j].getValue() == BARBERIAN) {
                     componentView[i][j].setImage(BARBERIAN.getActionImage());
                 }
 
