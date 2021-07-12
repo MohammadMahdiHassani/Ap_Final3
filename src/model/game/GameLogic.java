588876c9085742853342688211b60061fc1d05ea
Moxitron <70322560+moxitron@users.noreply.github.com>
2021-07-12 16:17:03 +0430

    adding updateBoard method + switch case to updateCards in GameLogic.java

--- src/model/game/GameLogic.java
+++ src/model/game/GameLogic.java
@@ -14,22 +14,30 @@ public class GameLogic {
     private boolean playerMoved ;
 
     public GameLogic() {
-        this.model = ArenaModel.getModel();
-        data = model.gameData ;
         playerMoved = false ;
+        botlevel = Botlevel.RANDOME ;
+    }
+    public void preprocessLogic(){
+        model = ArenaModel.arenaModel;
+        data = ArenaModel.arenaModel.gameData;
     }
     public void executeLogic(){
 
         checkForPlayerMove();
-        updateBoardCards();
+        updateCards();
+        updateBoard();
         if(playerMoved){
-            executeBot() ;
+            executeBot();
+            playerMoved = false ;
         }
     }
+
+
     private void checkForPlayerMove(){
         if(currPoint != null && currCard != null){
             if(point2cellValue(currPoint) == null){
                 currCard.setPoint(currPoint);
+                data.boardElements.add(currCard);
                 playerMoved = true ;
                 currCard = null ;
                 currPoint = null ;
@@ -38,25 +46,10 @@ public class GameLogic {
     }
 
 
-    public void setBotlevel(Botlevel botlevel) {
-        this.botlevel = botlevel;
-    }
     private GameElement point2cellValue(Point2D point){
         return model.cellValues[(int)point.getY()][(int)point.getX()];
     }
-    public void setCurrPoint(Point2D currPoint) {
-        if(!(currPoint.getY()>=11 && currPoint.getY()<=17))
-            return ;
-        else if(currCard != null)
-            return ;
-        else
-            this.currPoint = currPoint;
-    }
 
-    public void setCurrCard(Card currCard) {
-        if(!data.playedCards.contains(currCard))
-            this.currCard = currCard;
-    }
     private void executeBot(){
         switch(botlevel){
             case RANDOME:
@@ -67,19 +60,27 @@ public class GameLogic {
         }
     }
 
-    private void updateBoardCards(){
+
+    private void updateCards(){
 
 
-        for(GameElement m : data.playedCards){
+        for(GameElement m : data.boardElements){
             if(m instanceof Card){
-//                Point2D mPoint = m.getPoint() ;
-//                if(mPoint.getY() > 11){
-//                    if(mPoint.getX())
-//
-//
-//
-//                }
 
+                switch(m.getValue()){
+                    case GIANT:
+
+                    case ARCHER:
+                    case BARBERIAN:
+                    case MINI_PEKA:
+                    case WIZARD:
+                    case VALKYRIE:
+                    case CANNON:
+                    case INFERNO:
+                    case FIREBALL:
+                    case RAGE:
+                    case ARROWS:
+                }
 
             }
             else
@@ -89,14 +90,33 @@ public class GameLogic {
 
         }
 
-
-
-
-
-
-
-
-
+    }
+    private void updateBoard() {
+        model.cellValues = new GameElement[model.rowCount][model.columnCount];
+        for (GameElement i : data.boardElements) {
+            model.cellValues[(int) i.getPoint().getY()][(int) i.getPoint().getX()] = i;
+        }
 
     }
+
+    public void setBotlevel(Botlevel botlevel) {
+        this.botlevel = botlevel;
+    }
+    public void setCurrPoint(Point2D currPoint) {
+        if(!(currPoint.getY()>=11 && currPoint.getY()<=17))
+            return ;
+        else if(currCard == null)
+            return ;
+        else
+            this.currPoint = currPoint;
+    }
+    public void setCurrCard(Card currCard) {
+        if(!data.boardElements.contains(currCard))
+            this.currCard = currCard;
+    }
+
+
+
+
+
 }
